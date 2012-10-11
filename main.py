a346671139819f4b8c9b0e30e58db84c895630ee
Julius Bier Kirkegaard <juliusbierk@gmail.com>
2012-10-12 01:21:15 +0200

    Simple, working version.

--- main.py
+++ main.py
@@ -15,41 +15,79 @@ class Warrior:
         self.reads = [pointer]
         self.name = name
         self.variables = {}
+        self.alive = True
 
     def evaluate(self, expr):
         try:
-            result = str(eval(expr_r, self.variables))
+            result = str(eval(expr, self.variables))
             return result
         except:
-            print 'Warning to ' + self.name + '! Expression: "'+expr+' coult not be evaluated. Turn skipped.'
+            print '1: Warning to ' + self.name + '! Expression: "'+expr+'" coult not be evaluated. Turn skipped.'
         return ''
 
     def next_move(self, board):
-        do = board[ self.read[self.next_ptr_read] ]
+        do = board[ self.reads[self.next_ptr_read] ]
+
+        print 'Now doing: '+do+' for warrior '+self.name+'.'
         
         ##### Now analyze dat language:
         goto_next = 1
+        next_ptr = 1
         #Goto:
         if do.startswith('goto'):
             stripped = do.replace('goto','').strip()
-            goto_next = self.evaluate(stripped)
+            try:
+                goto_next = int(self.evaluate(stripped))
+            except:
+                print '2: Warning to ' + self.name + '! Expression: "'+do+'" coult not be evaluated. Turn skipped.'
         
         #Define or change variable:
-        elif '=' in do:
-            var = do[ :do.index('=') ]
+        elif '=' in do.replace('==',''):
+            var = do[ :do.index('=') ].strip()
             expr = do[ do.index('=')+1: ]
-            self.variables[var] = self.evaluate(expr)
+            self.variables[var] = int(self.evaluate(expr))
+
+        #Copy:
+        elif do.startswith('copy'):
+            stripped = do.replace(' ','').replace('copy(','').strip().rstrip(')')
+            expr1 = ''
+            expr2 = ''
+            paren = 0
+            bracket = 0
+            first = True
+            for s in stripped:
+                if first:
+                    if s == ',' and paren == 0 and bracket == 0:
+                        first = False
+                    else:
+                        expr1 += s
+                else:
+                    expr2 += s
+            
+            from_index = ( self.reads[self.next_ptr_read] + int(self.evaluate(expr1)) ) % len(board)
+            to_index = ( self.reads[self.next_ptr_read] + int(self.evaluate(expr2)) ) % len(board)
+            board[to_index] = board[from_index]
+
+        #Something else counts as return:
+        else:
+            del self.reads[self.next_ptr_read]
+            next_ptr = 0
+            if len(self.reads) == 0:
+                alive = False
+                return False
 
 
         ## And go to next statement:
-        self.read += goto_next
-        if self.read >= len(board):
-            self.read = 0
+        self.reads[self.next_ptr_read] += goto_next
+        if self.reads[self.next_ptr_read] >= len(board):
+            self.reads[self.next_ptr_read] = 0
 
         ##### Go to next thread:
-        self.next_ptr_read += 1
-        if self.next_ptr_read >= len(self.read):
+        self.next_ptr_read += next_ptr
+        if self.next_ptr_read >= len(self.reads):
             self.next_ptr_read = 0
+
+        return True
 
 class WarBoard:
     def __init__(self, f_warrior1, f_warrior2, N=300, m=100):
@@ -61,6 +99,8 @@ class WarBoard:
 
         self.warrior1 = Warrior(0, self.warrior1_name)
         self.warrior2 = Warrior(self.warrior2_start, self.warrior2_name)
+
+        self.turn = randint(1,2)
 
     def __str__(self):
         out = ''
@@ -104,7 +144,22 @@ class WarBoard:
             board[self.warrior2_start + i] = s
         
         return board
+    
+    def next_move(self):
+        if self.turn == 1:
+            if not self.warrior1.next_move(self.board):
+                print '\n\nWarrior 2 wins!\n\n'
+                exit()
+        if self.turn == 2:
+            if not self.warrior2.next_move(self.board):
+                print '\n\nWarrior 1 wins!\n\n'
+                exit()
         
+        self.turn = 1 if self.turn == 2 else 2
+        
+        print self, len(self.board)
+
 #### DEBUGGING AND TESTING:
 test = WarBoard('warrior1.hlw', 'warrior2.hlw')
-print test
+for i in range(1,500):
+    test.next_move()
